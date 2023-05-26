9f73e9125764a143d010ff1e8ca1d54ead416144
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-05-26 09:48:22 +0500

    import langs from translate.js module

--- src/js/modules/alpine.js
+++ src/js/modules/alpine.js
@@ -1,38 +1,36 @@
+import langs from './translate'
+
 document.addEventListener('alpine:init', () => {
 	Alpine.data('data', () => ({
-        param: new URLSearchParams(location.search.substring(1)).get('lang'),
-        async init() {            
-            await fetch('data/translate.json')
-            .then(response => response.json())
-            .then(async data => {
-                if(!this.param){
-                    this.lang = await data.ru
-                    this.param = 'ru'
-                }else if(!Object.keys(data).includes(this.param)){
-                    this.lang = await data.ru
-                    this.param = 'ru'
-                }else{
-                    this.lang = await data[this.param]
-                }
-                console.log(this.param);
-            })            
-        },
-        lang: {},
-        findWords(str, key, slice = 0, target = ' '){
-            if(str !== undefined){
-                try {
-                    if(str){
-                        if(slice){
-                            return str.split(target).slice(slice).join(' ')
-                        }
-                        return str.split(target)[key];
-                    }
-                    
-                } catch (error) {
-                   console.log(error); 
-                }
-            }
-        }
+		param: new URLSearchParams(location.search.substring(1)).get('lang'),
+		langs: JSON.parse(langs),
+		init() {
+			if(!this.param){
+				this.lang = this.langs.ru
+				this.param = 'ru'
+			}else if(!Object.keys(this.langs).includes(this.param)){
+				this.lang = this.langs.ru
+				this.param = 'ru'
+			}else{
+				this.lang = this.langs[this.param]
+			}
+		},
+		lang: {},
+		findWords(str, key, slice = 0, target = ' '){
+			if(str !== undefined){
+				try {
+					if(str){
+						if(slice){
+							return str.split(target).slice(slice).join(' ')
+						}
+						return str.split(target)[key];
+					}
+					
+				} catch (error) {
+				   console.log(error); 
+				}
+			}
+		}
 	}))
 	Alpine.store('state', {
 		isModalOpen: false,
