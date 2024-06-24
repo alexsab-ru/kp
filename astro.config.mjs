23be0c1a99de913f97e48d3c287cb06479ea6317
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-06-24 13:32:49 +0500

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import alpinejs from '@astrojs/alpinejs';
+// import sitemap from "@astrojs/sitemap";
+// import robots from "astro-robots";
+// import mdx from "@astrojs/mdx";
+// import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		tailwind(),
+		// sitemap(),
+		// robots({
+		// 	policy: [
+		// 		{
+		// 			userAgent: ["*"],
+		// 			allow: ["/"],
+		// 			disallow: ["/?*"],
+		// 		},
+		// 	  ],
+		// }),
+		alpinejs(),
+		// mdx(),
+		// icon(),
+	],
+	redirects: {},
+	site: 'https://serm-karma.ru',
+	base: '/'
+});
